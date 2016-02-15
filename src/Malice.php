a6b1e6d71582f9213d1135e7a90c544e4083429b
David Alsbury <david@alsbury.com>
2016-02-15 00:53:56 -0800

    Initial commit

---
+++ src/Malice.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Alsbury\Malice;
+
+use Codeception\Module as CodeceptionModule;
+use Codeception\TestCase;
+
+
+class Malice extends CodeceptionModule
+{
+    protected $config = [
+    ];
+
+    public function _before(TestCase $test)
+    {
+
+    }
+
+    public function _after(TestCase $test)
+    {
+
+    }
+
+
+}
