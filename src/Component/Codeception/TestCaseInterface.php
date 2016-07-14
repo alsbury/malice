5dcb77b79fe0101bf86b72d32cbc4e16c4967b74
Rob Kreisel <nocom.gen@gmail.com>
2016-07-14 08:45:07 -0500

    stripping interface entirely

--- src/Component/Codeception/TestCaseInterface.php
+++ src/Component/Codeception/TestCaseInterface.php
@@ -2,12 +2,7 @@
 
 namespace Alsbury\Malice\Component\Codeception;
 
-use Alsbury\Malice\Component\Fixtures\FixtureConfig;
-
 interface TestCaseInterface
 {
-    /**
-     * @return FixtureConfig|null
-     */
-    public function _getFixtureConfig();
+
 }
